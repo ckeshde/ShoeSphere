6fa56d79a65376fe7442170da33d2e53a94c6522
een <809926788@qq.com>
2025-04-25 23:44:38 +1000

    modify firebaseConfiguration

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,12 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+//  Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCoPoHRoGvrWMvgE4GMuj5pHMmPo8RK27I",
   authDomain: "shoesphere-d37fd.firebaseapp.com",
