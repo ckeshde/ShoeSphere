e2dbf54b9680afd094d541404a9fa8810c69ed53
een <809926788@qq.com>
2025-05-10 21:25:45 +1000

    Optimized add reviews page

--- app/addreview.tsx
+++ app/addreview.tsx
@@ -2,32 +2,32 @@ import { useRouter } from 'expo-router';
 import {addDoc, collection, Timestamp} from 'firebase/firestore';
 import React , {useState} from 'react';
 import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import {db} from './firebaseConfig';
+import {db, auth} from './firebaseConfig';
 
 
 export default function AddReviewScreen() {
 
-    const [username, setUsername] = useState('');
+    // const [username, setUsername] = useState('');
     const [content, setContent] = useState('');
     const [rating, setRating] = useState('');
     const router = useRouter();
 
     const handleSubmit = async () => {
-        if (!username || !content || !rating) {
+        if ( !content || !rating) {
             Alert.alert('Please fill in all fields');
             return;
         }
 
+        const user = auth.currentUser;
         try {
             await addDoc(collection(db, 'reviews'), {
-                username,
-                content,
+                username: user?.email?.split('@')[0] || 'guest', //Automatically obtain the email prefix
                 rating: Number(rating),
+                content,
                 createdAt: Timestamp.now(),
             });
             Alert.alert('The review was submitted successfully!')
             router.back();
-
         } catch (error) {
             console.error('Failded to add a review:', error);
             Alert.alert('Submit failure')
@@ -37,8 +37,8 @@ export default function AddReviewScreen() {
 
     return (
         <View style={styles.container}>
-          <Text style={styles.label}>User name</Text>
-          <TextInput style={styles.input} value={username} onChangeText={setUsername} placeholder="Please enter your name" />
+          {/* <Text style={styles.label}>User name</Text> */}
+          {/* <TextInput style={styles.input} value={username} onChangeText={setUsername} placeholder="Please enter your name" /> */}
           
           <Text style={styles.label}>Rating（1-5）</Text>
           <TextInput
@@ -58,20 +58,20 @@ export default function AddReviewScreen() {
             multiline
           />
     
-          <Button title="Submit review" onPress={handleSubmit} />
+          <Button title="Submit" onPress={handleSubmit} />
         </View>
       );
     }
     
     const styles = StyleSheet.create({
-      container: { flex: 1, padding: 16, backgroundColor: '#fff' },
-      label: { fontSize: 16, marginTop: 12 },
+      container: { flex: 1, padding: 20, backgroundColor: '#fff' },
+      label: { fontSize: 16, marginTop: 16 },
       input: {
         borderWidth: 1,
         borderColor: '#ccc',
-        paddingHorizontal: 10,
-        paddingVertical: 8,
-        borderRadius: 6,
-        marginTop: 4,
+        paddingHorizontal: 12,
+        paddingVertical: 10,
+        borderRadius: 8,
+        marginTop: 8,
       },
     });
